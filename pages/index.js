cd4adebfc002c6cd77cddfccfce37e817ff0e738
PedroEmilioMartinelli <pedro@Pedro.>
2025-02-16 22:02:16 -0300

    adicona a página 'pages', com o texto certo

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,8 @@ import { Html } from "next/document";
 
 function Home() {
     return <h2>Fala Henrique tudo certo? como ta ai na Espanha?
-    coloquei esse hospedei esse site aqui na web show de bola, foi sofrido, mas consegui </h2>
+    coloquei hospedei esse site aqui na web show de bola, foi sofrido, mas consegui </h2>
 
 }
 
 export default Home;
-

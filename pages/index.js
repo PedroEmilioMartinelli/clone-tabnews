82639dd4766f51fcd750131eb3364db56dcad3ec
PedroEmilioMartinelli <pedro@Pedro.>
2025-02-17 18:49:45 -0300

    desafio

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { Html } from "next/document";
 
 function Home() {
-    return <h2>Esse site aqui está em construçao, aguarde até que ele esteje pronto ou seja atualizado </h2>
+    return <h2>Oi mor eu te amo, e tu deve ta pensando nesse momento "que boiolagem" but fds</h2>
 
 }
 
